c3d9608a12777eaa2fc8d5a907cc6f7a07a7a87c
Ayman Omer <aymangebrilomer@gmail.com>
2021-01-28 21:15:09 -0500

    new update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import "./App.css";
 import Header from "./componets/Header";
 import MapindData from "./componets/MapindData";
-import FatchData from "./service/FatchData";
-import Profile from "./componets/Profile";
-import { useEffect, useState } from "react";
 
 function App() {
   return (
